59ea11779d0576f015e85eae509922929111e25b
Mhadi Ahmed <mhadiahmed@users.noreply.github.com>
2023-01-09 22:43:04 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/mahdi-portfolio/" : "/",
-};
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/mahdi-portfolio/'
+    : '/'
+}
